514b815a0fbf86e6e691534429b3e8e257d01cb5
Wasil Khan <wasilkhan914@gmail.com>
2024-12-20 12:05:23 +0530

    geneform

---
+++ client/src/firebase.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY
+  ,
+  authDomain: "geneforms-dee30.firebaseapp.com",
+  projectId: "geneforms-dee30",
+  storageBucket: "geneforms-dee30.firebasestorage.app",
+  messagingSenderId: "1075155791996",
+  appId: "1:1075155791996:web:59c5233f244b70821d19f9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
