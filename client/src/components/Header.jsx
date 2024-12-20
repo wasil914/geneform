51b70ff04b986431d641fde8ed35199b0c288c06
Wasil Khan <wasilkhan914@gmail.com>
2024-12-20 13:59:08 +0530

    geneform-app

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
     <div className='bg-slate-200'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link to='/'>
-          <h1 className='font-bold'>Auth App</h1>
+          <h1 className='font-bold'>GeneForm-S</h1>
         </Link>
         <ul className='flex gap-4'>
           <Link to='/'>
